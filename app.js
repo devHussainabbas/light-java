bf0d476479334042b8d27b93365bcd4b94c55890
devHussainabbas <03555abc@gmail.com>
2024-05-13 21:45:24 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function turnOnLight() {
+    document.getElementById("hi").style.color = "orange"; 
+}
+
+function turnOffLight() {
+    document.getElementById("hi").style.color = "black"; }
+
+
+  
+
+
